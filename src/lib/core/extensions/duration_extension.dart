30e9174445117531375ce87ae8090414d694cb85
Quang Phan <oclockvn@gmail.com>
2020-04-15 11:51:22 +0700

    display 0 if negative duration

--- src/lib/core/extensions/duration_extension.dart
+++ src/lib/core/extensions/duration_extension.dart
@@ -1,10 +1,12 @@
+import 'dart:math';
+
 extension DurationExtension on Duration {
   String print() {
     if (this == null) {
       return '00:00';
     }
 
-    String twoDigits(int n) => (n >= 10) ? '$n' : '0$n';
+    String twoDigits(int n) => (n >= 10) ? '$n' : '0${max(0, n)}';
 
     String twoDigitMinutes = twoDigits(this.inMinutes.remainder(60));
     String twoDigitSeconds = twoDigits(this.inSeconds.remainder(60));
